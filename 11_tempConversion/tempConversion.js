e48ca43c35fc887c632d3d34a5fd74247b578614
Amir Hooka <amirhooka@Amirs-MacBook-Pro.local>
2025-10-27 21:07:41 +0330

    complete excercise 06 to 11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let cels = (fah - 32) * (5/9);
+  cels = Math.round(cels * 10) / 10;
+  return cels;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) { 
+    let fahren = (cel * (9/5))+ 32 ;
+    fahren = Math.round(fahren * 10) / 10;
+    return fahren
 };
 
+
+console.log(convertToFahrenheit(12.32321));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
